634209513e3ff25bdd2420d2f400c4f6db4fade0
manish <manumi296@gmail.com>
2023-01-09 12:05:08 +0530

    new depl

--- sensor/constant/env_variable.py
+++ sensor/constant/env_variable.py
@@ -1,7 +1,9 @@
 import os
 
 
-MONGO_DB_URL_KEY = os.getenv(mongo_db_url)
+MONGO_DB_URL_KEY = os.getenv("MONGO_DB_URL")
+#MONGO_DB_URL_KEY = "mongodb+srv://trialmongo:version2022@cluster0.2gugo3n.mongodb.net/test"
+
 
 
 AWS_ACCESS_KEY_ID_ENV_KEY="AWS_ACCESS_KEY_ID"
