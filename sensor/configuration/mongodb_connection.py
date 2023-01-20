2335dc136d8c6fd25aa4d34b75f47f9b3ad29ad1
manish <manumi296@gmail.com>
2023-01-20 17:35:42 +0530

    'nero_lab test'

--- sensor/configuration/mongodb_connection.py
+++ sensor/configuration/mongodb_connection.py
@@ -14,6 +14,8 @@ class MongoDBClient:
             if MongoDBClient.client is None:
                 # mongo_db_url = os.getenv(MONGODB_URL_KEY)
                 mongo_db_url = MONGO_DB_URL_KEY
+                if mongo_db_url is None:
+                    raise Exception(f"Environment key: {MONGO_DB_URL_ENV_KEY} is not set.")
 
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
